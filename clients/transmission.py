db5d89d8409be48b21be8e1a9b111f8377e08f43
Lars De Volder <36573562+beverneus@users.noreply.github.com>
2025-06-23 01:15:47 +0200

    setup imports based on qbittorrent client

---
+++ clients/transmission.py
@@ -0,0 +1,6 @@
+import transmission_rpc
+from typing import Union
+
+from helpers.config import SpeedrrConfig, ClientConfig
+from helpers.log_loader import logger
+from helpers.bit_convert import bit_conv
